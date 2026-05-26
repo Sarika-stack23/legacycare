9dbfb2d0bd541395b7687c057ca37cdbfe5ab428
Sarika-stack23 <jivrajikasarika2005@gmail.com>
2026-05-26 17:09:09 +0530

    v7

--- server/server.js
+++ server/server.js
@@ -12,7 +12,16 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://legacycare-frontend.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
